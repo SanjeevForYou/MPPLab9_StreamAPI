611d8d4003f4f2d2889299f6ae5ac850827b6605
SanjeevForYou <yoursanjeev0@gmail.com>
2016-06-09 11:59:06 -0500

    first file Stream practice

---
+++ src/lab9_streamAPI/SteramTest.java
@@ -0,0 +1,66 @@
+package lab9_streamAPI;
+
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class SteramTest {
+	
+	public static Stream<Character> charecterStream(String s)
+	{
+		List<Character> result = new ArrayList<Character>();
+		for(char c : s.toCharArray())
+		{
+		   result.add(c);
+		}
+		return result.stream();
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        List<Student> stList = new ArrayList<Student>();
+        
+       stList.add(new Student("Sanjeev"));
+       stList.add(new Student("Maya"));
+       stList.add(new Student("Maryam"));
+       
+       long count = stList.stream().filter(s -> s.getName().startsWith("M")).count();
+       System.out.println(count);
+       
+       Stream<String> echos = Stream.generate(() -> "echo");
+       Stream<Double> randoms = Stream.generate(Math::random); //generates stream of Random number
+       List<Double> result = randoms.limit(100).skip(10).collect(Collectors.toList());
+       Iterator<Double> di = result.iterator();
+       while(di.hasNext())
+       {
+    	   System.out.println(di.next());
+       }
+       
+       
+       Stream<BigInteger> number = Stream.iterate(BigInteger.ONE, n -> n.add(BigInteger.ONE)); //generates stream of Random number
+       List<BigInteger> resel = number.limit(100).skip(10).collect(Collectors.toList());
+       Iterator<BigInteger> di1 = resel.iterator();
+       while(di1.hasNext())
+       {
+    	   System.out.println(di1.next());
+       }
+       
+       //Generating stream of String and transfering to list
+       List<Character> chList = charecterStream("Sanjeev").limit(3).skip(1).collect(Collectors.toList());
+      
+       Iterator<Character> chl = chList.iterator();
+    		   while(chl.hasNext())
+    	       {
+    	    	   System.out.println(chl.next());
+    	       }
+       
+    		System.out.println(chList);   
+    		
+    		 List<Character> combinedList = Stream.concat(charecterStream("San"), charecterStream("Bhu")).collect(Collectors.toList());
+    		 System.out.println(combinedList);
+	}
+
+}
---
+++ src/lab9_streamAPI/Student.java
@@ -0,0 +1,16 @@
+package lab9_streamAPI;
+
+public class Student {
+
+	private String name;
+	
+	public Student(String nam)
+	{
+		this.name= nam;
+	}
+	
+	public String getName()
+	{
+		return name;
+	}
+}
